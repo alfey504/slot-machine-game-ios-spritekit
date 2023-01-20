5a19374c5da50674be142a3c4a57edd0f90bd8f6
alfey504 <alfey504@gmail.com>
2023-01-20 06:06:28 -0500

    Initial Commit

---
+++ Slot Machine GameUITests/Slot_Machine_GameUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Slot_Machine_GameUITestsLaunchTests.swift
+//  Slot Machine GameUITests
+//
+//  Created by Abraham Alfred Babu on 2023-01-20.
+//
+
+import XCTest
+
+final class Slot_Machine_GameUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
